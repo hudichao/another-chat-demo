129042142478685cdac0e8e79b0aad5c55baa464
胡迪超 <hu.dichao@gmail.com>
2016-06-16 10:39:31 +0800

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+
+app.get("/", function(req, res) {
+  // res.send("<h1>hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+
+  // 每个socket都会fire一个disconnect event
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+  
+});
+
+
+
+http.listen(3000, function() {
+  console.log("listen on 3000");
+})
